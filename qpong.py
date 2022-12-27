db586e8e3e1dc785031d56771538f2d7396b1420
jvscursulim <josevictor.s.scursulim@gmail.com>
2022-12-27 14:51:39 -0300

    updates on qpong file

--- qpong.py
+++ qpong.py
@@ -1,5 +1,6 @@
 import pygame
-from assets import WINDOW_WIDTH, WINDOW_HEIGHT, FPS
+from assets import WINDOW_WIDTH, WINDOW_HEIGHT, FPS, FIELD_HEIGHT
+from assets import CircuitGrid, ui
 
 pygame.init()
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -8,11 +9,22 @@ clock = pygame.time.Clock()
 
 def main():
 
+    # initialize game
+    circuit_grid = CircuitGrid(xpos=5, ypos=FIELD_HEIGHT)
+
     exit = False
     while not exit:
+        # update game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit = True
+            elif event.type == pygame.KEYDOWN:
+                circuit_grid.handle_input(event.key)
+
+        # draw game
+        circuit_grid.draw(screen)
+        ui.draw_statevector_grid(screen)
+        pygame.display.flip()
 
         # set game framerate
         clock.tick(FPS)
